88280749c5516e75641479aed215c90c6731dd86
Faustov_AA <recettivo@gmail.com>
2026-07-01 12:17:56 +0300

    data base agi_prototype in PostgreSQL

---
+++ data_base/instinct_patterns_202607011213.sql
@@ -0,0 +1,2 @@
+INSERT INTO agi_prototype.instinct_patterns (pattern,actions,success_rate,usage_count,last_used) VALUES
+	 ('{"signals": {"sound": "loud crash", "vision": "bright_flash"}}','{"action": "run_away"}',NULL,NULL,NULL);
